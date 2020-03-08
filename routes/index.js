5d233b415da680e2d6929d7f23931243ca045581
Dasha Day Hisoler <dhisoler@hawk.iit.edu>
2020-03-07 21:25:29 -0600

    Add router to login page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Travel Itinerary' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { page_name: 'Registration' });
+});
+
 module.exports = router;
