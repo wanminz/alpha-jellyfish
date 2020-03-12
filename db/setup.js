f01f2f4ad99b34a2b377c70dd5ad752e29d9ecec
WANMIN ZHANG <wzhang90@hawk.iit.edu>
2020-03-11 20:19:43 -0500

    Create a table called trips

--- db/setup.js
+++ db/setup.js
@@ -2,16 +2,18 @@ const db = require('./connect.js').connect();
 
 db.serialize(() => {
   let create_table = `CREATE TABLE trips
-  ( id INTEGER PRIMARY KEY AUTOINCREMENT    
-  )`;
+  ( id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(50),
+    start_date TEXT,
+    return_date TEXT,
+    transport_mode VARCHAR(50)
+  )`
   db.run(create_table, (err) => {
     if (err) {
       console.error(err.message);
     }
   });
 });
-
-
 
 db.close((err) => {
   if (err) {
