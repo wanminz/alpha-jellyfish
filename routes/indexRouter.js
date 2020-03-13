b3a528c8b604409d3ee6ea1728bc08d09ad7bda0
Dasha Day Hisoler <dhisoler@hawk.iit.edu>
2020-03-13 15:19:03 -0500

    Add router to flight.pug to display flight information

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,4 +7,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Travel Itinerary' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { page_name: 'Registration' });
+});
+
+router.get('/flight', async function(req, res, next) {
+  try {
+    let async_data = await flight.getFlightData();
+    res.render('flight', {title: 'Flight Information', async_data});
+
+  } catch (err) {
+    res.status(err.response.status)
+    console.log("Caught error " + err.message);
+  }
+});
+
 module.exports = router;
