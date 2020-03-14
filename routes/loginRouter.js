b948264ec06d629e4219deebca92295e75543c15
Dasha Day Hisoler <dhisoler@hawk.iit.edu>
2020-03-13 19:51:09 -0500

    Remove loginRouter.js

--- routes/loginRouter.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/login', function(req, res, next) {
-  res.render('login', { page_name: 'Registration' });
-});
-
-module.exports = router;
